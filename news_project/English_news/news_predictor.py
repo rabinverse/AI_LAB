a3a793399e643b8556ebb55eb28ad232e3295cb7
rabinverse <poudelrabin2004@gmail.com>
2026-02-05 00:08:56 +0545

    Refactor code structure for improved readability and maintainability

--- news_project/English_news/news_predictor.py
+++ news_project/English_news/news_predictor.py
@@ -1,11 +1,11 @@
-import streamlit as st 
-import joblib 
+import streamlit as st
+import joblib
 
 
-model=joblib.load("news_category_predictor.joblib")
+model = joblib.load("english_news_category_predictor.joblib")
 
-user_input=st.text_area("Enter the news to predict its category").strip()
+user_input = st.text_area("Enter the news to predict its category").strip()
 
 if st.button("predict"):
-    output=model.predict([user_input])[0]
+    output = model.predict([user_input])[0]
     st.success(f"🙏\n{output}")
--- news_project/Nepali_news/nepali_news_predictor.py
+++ news_project/Nepali_news/nepali_news_predictor.py
@@ -4,11 +4,13 @@ import joblib
 st.title("समाचार वर्गीकरण प्रणाली")
 
 model = joblib.load("nepali_news_category_predictor.joblib")
-st.markdown("""the category it predicts ['विचार', 'देश', 'खेलकुद', 'मनोरञ्जन', 'प्रवास', 'साहित्य',
-       'सूचना प्रविधि', 'स्वास्थ्य', 'विश्व']""")
-user_input = st.text_area("यहाँ समाचारको पाठ लेख्नुहोस्:")
+st.markdown(
+    """पूर्वानुमान गरिने समाचारका वर्गहरू : :orange['विचार', 'देश', 'खेलकुद', 'मनोरञ्जन', 'प्रवास', 'साहित्य',
+       'सूचना प्रविधि', 'स्वास्थ्य', 'विश्व']"""
+)
+st.divider()
+user_input = st.text_area("यहाँ समाचार लेख्नुहोस्:")
 st.divider()
 if st.button("पूर्वानुमान गर्नुहोस्"):
     output = model.predict([user_input])[0]
     st.success(f" यो समाचारको वर्ग :violet[\n{output}] हो")
-"."
